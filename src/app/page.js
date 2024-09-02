64cac998296a630587ad11cc0f39a5c32d07f057
Jeremy <e1155908@u.nus.edu>
2024-09-02 14:32:27 +0800

    Add skeleton project cards into main page

--- src/app/page.js
+++ src/app/page.js
@@ -18,61 +18,111 @@ export default function Home() {
             <nav className="fixed top-0 w-full bg-zinc-950 shadow-2xl z-50">
                 <ul className="flex justify-end p-4">
                     <li>
-                        <a onClick={() => scrollToSection('home')} className="text-white font-bold m-6 cursor-pointer">Home</a>
+                        <a onClick={() => scrollToSection('home')}
+                           className="text-white font-bold m-6 cursor-pointer">Home</a>
                     </li>
                     <li>
-                        <a onClick={() => scrollToSection('about')} className="text-white font-bold m-6 cursor-pointer">About Me</a>
+                        <a onClick={() => scrollToSection('about')} className="text-white font-bold m-6 cursor-pointer">About
+                            Me</a>
                     </li>
                     <li>
-                        <a onClick={() => scrollToSection('projects')} className="text-white font-bold m-6 cursor-pointer">Projects</a>
+                        <a onClick={() => scrollToSection('projects')}
+                           className="text-white font-bold m-6 cursor-pointer">Projects</a>
+                    </li>
+                    <li>
+                        <a onClick={() => scrollToSection('contacts')}
+                           className="text-white font-bold m-6 cursor-pointer">Contacts</a>
                     </li>
                 </ul>
             </nav>
 
             <div id="home" className="min-h-screen flex items-center justify-center bg-black">
                 <motion.div
-                    initial={{ opacity: 0, y: -50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
+                    initial={{opacity: 0, y: -50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.5}}
                 >
                     <h1 className="text-5xl font-bold text-white">Welcome to My Website</h1>
                     <p className="text-2xl text-white">
-                        Hi. I am Jeremy, a Computer Science Undergraduate from the National University of Singapore
+                        Hi. I am Jeremy, an aspiring Software engineer
                     </p>
                 </motion.div>
             </div>
 
             {/* Separator */}
-            <hr className="border-t-4 border-white mx-auto rounded-2xl w-3/4"/>
+            <hr className="border-t-4 border-white mx-auto rounded-2xl w-3/4 my-10"/>
 
             <div id="about" className="min-h-screen flex items-center justify-center bg-black">
                 <motion.div
-                    initial={{ opacity: 0, x: -100 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.5 }}
+                    initial={{opacity: 0, x: -100}}
+                    animate={{opacity: 1, x: 0}}
+                    transition={{duration: 0.5}}
                 >
                     <h2 className="text-4xl font-semibold text-white">About Me</h2>
-                    <p className="mt-4 text-xl text-white">I am a passionate developer...</p>
+                    <p className="mt-4 text-xl text-white">
+                        I am Jeremy, a Computer Science Undergraduate from the National University of Singapore.
+                        I am a passionate developer...
+                    </p>
                 </motion.div>
             </div>
 
             {/* Separator */}
-            <hr className="border-t-4 border-white mx-auto rounded-2xl w-3/4"/>
+            <hr className="border-t-4 border-white mx-auto rounded-2xl w-3/4 my-10"/>
 
             <div id="projects" className="min-h-screen flex items-center justify-center bg-black">
                 <motion.div
-                    initial={{ opacity: 0, y: 50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
+                    initial={{opacity: 0, y: 50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.5}}
                 >
                     <h2 className="text-4xl font-semibold text-white">My Projects</h2>
                     <p className="mt-4 text-xl text-white">Here are some of my recent works...</p>
 
-                    <div className="rounded-2xl bg-white w-96 h-52 flex justify-center items-center">
-                        <p className="text-black">Project #1</p>
+                    <div className="flex justify-around bg-gray-700">
+                        <div className="rounded-2xl bg-white w-96 h-52 flex justify-center items-center m-14">
+                            <p className="text-black">Project #1</p>
+                        </div>
+
+                        <p className="m-5 mt-12 bg-gray-400"> Some description of Project #1</p>
+                    </div>
+
+
+                    <div className="flex justify-around bg-gray-700 mt-10">
+                        <p className="m-5 mt-12 bg-gray-400"> Some description of Project #2</p>
+
+                        <div className="rounded-2xl bg-white w-96 h-52 flex justify-center items-center m-14">
+                            <p className="text-black">Project #2</p>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-around bg-gray-700 mt-10">
+                        <div className="rounded-2xl bg-white w-96 h-52 flex justify-center items-center m-14">
+                            <p className="text-black">Project #3</p>
+                        </div>
+
+                        <p className="m-5 mt-12 bg-gray-400"> Some description of Project #3</p>
                     </div>
                 </motion.div>
             </div>
+
+            {/* Separator */}
+            <hr className="border-t-4 border-white mx-auto rounded-2xl w-3/4 my-10"/>
+
+            <div id="contacts" className="min-h-screen flex items-center justify-center bg-black">
+                <motion.div
+                    initial={{opacity: 0, x: -100}}
+                    animate={{opacity: 1, x: 0}}
+                    transition={{duration: 0.5}}
+                >
+                    <h2 className="text-4xl font-semibold text-white">My contacts</h2>
+                    <p className="mt-4 text-xl text-white">
+                        LinkedIn, Github, Email, Resume
+                        Contact me!
+                    </p>
+                </motion.div>
+            </div>
+
+
         </div>
     );
 }
