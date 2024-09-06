eda51970eca95276a4c7967fe98ec2711270de19
Jeremy <e1155908@u.nus.edu>
2024-09-06 15:04:39 +0800

    Add description and picture in home section

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,17 +1,33 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
     return (
         <div id="home" className="min-h-screen flex items-center justify-center bg-black">
             <motion.div
-                initial={{opacity: 0, y: -50}}
-                animate={{opacity: 1, y: 0}}
-                transition={{duration: 0.5}}
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className="text-center"
             >
-                <h1 className="text-5xl font-bold text-white">Welcome to My Website</h1>
-                <p className="text-2xl text-white">
-                    Hi. I am Jeremy, an aspiring Software engineer
-                </p>
+                <div className="flex justify-center mb-6">
+                    <div className="w-96 h-96 rounded-full overflow-hidden border-4 border-white">
+                        <img
+                            src="/me.png"
+                            alt="Jeremy"
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+
+                    <div className="mt-6 ml-8 flex flex-col items-center justify-center">
+                        <h1 className="text-6xl font-bold text-white self-start">Hello. I am Jeremy! </h1>
+                        <p className="mt-6 text-xl text-white self-start max-w-xl">
+                            an aspiring Full Stack Software engineer who is passionate about building softwares
+                            to make the world a better place!
+                        </p>
+                    </div>
+
+                </div>
+
             </motion.div>
         </div>
     );
