041be016cc329d833781f380ae76771f13d5789d
Jeremy <e1155908@u.nus.edu>
2024-09-12 02:16:17 +0800

    Add social links to contacts section
    Include Linkedin, Instagram and Github links in the contacts section so
    that viewers can have a quick access to my social links

--- src/app/components/Contacts.jsx
+++ src/app/components/Contacts.jsx
@@ -1,4 +1,5 @@
 import {motion} from "framer-motion";
+import {SocialIcon} from "react-social-icons";
 
 export default function Contacts() {
     return (
@@ -8,11 +9,19 @@ export default function Contacts() {
                 animate={{opacity: 1, x: 0}}
                 transition={{duration: 0.5}}
             >
-                <h2 className="text-4xl font-semibold text-white">Let&apos;s Connect!</h2>
-                <p className="mt-4 text-xl text-white">
-                    LinkedIn, Github, Email, Resume
-                    Contact me!
-                </p>
+                <h2 className="text-4xl font-semibold text-white mb-10 text-left">Let&apos;s Connect!</h2>
+                    <div
+                        className="justify-around px-8 py-4 flex flex-wrap gap-6 mt-4 bg-zinc-900 rounded-3xl max-w-md shadow-2xl">
+                        <div className="flex flex-col items-center">
+                            <SocialIcon url={"https://www.linkedin.com/in/jeremy-sim-0669b427a/"}/>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <SocialIcon url={"https://github.com/jereeemyyyy"}/>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <SocialIcon url={"https://www.instagram.com/jereeemyyyy/"}/>
+                        </div>
+                    </div>
             </motion.div>
         </div>
     );
