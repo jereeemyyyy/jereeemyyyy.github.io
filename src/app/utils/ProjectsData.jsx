8ae3f3ee04d1a6ed9213d0a277e6129e53f401a2
Jeremy <e1155908@u.nus.edu>
2024-11-29 14:16:10 +0800

    Add new utils directory
    Add utils directory to store information about projects. Makes the code
    more scalable in the future.

---
+++ src/app/utils/ProjectsData.jsx
@@ -0,0 +1,37 @@
+const projectsData = [
+    {
+        title: "ClientHub" ,
+        description: "An application that assist Financial Advisors in tracking the details of their clients by providing " +
+          "a platform to store and manage their clients' information.",
+        imageSrc: "",
+        techStack: ["java", "javafx", "github", "gradlew"],
+        githubUrl: "https://github.com/jereeemyyyy/tp",
+
+    },
+    {
+        title: "SaveLah",
+        description:
+            "A simple financial tracker app that streamlines the process of tracking your expenses by providing real time updates of your transactions.",
+        imageSrc: "/savelah2.png",
+        techStack: ["reactjs", "supabase", "github", "tailwindcss"],
+        githubUrl: "https://github.com/jereeemyyyy/savelah-project",
+    },
+    {
+        title: "CryptoPalace",
+        description:
+            "A website that tracks the prices of all the cryptocurrencies and provided relevant information about the currencies. The CoinGecko API was used in this project to retrieve real time data of the cryptocurrencies.",
+        imageSrc: "/cryptopalace.png",
+        techStack: ["reactjs", "github"],
+        githubUrl: "https://github.com/jereeemyyyy/cryptopalace",
+    },
+    {
+        title: "Simple Web Forum",
+        description:
+            "A simple web forum page that contains basic CRUD functionalities, that allows user to create, read, update and delete posts. My first ever project!",
+        imageSrc: "",
+        techStack: ["reactjs", "postgresql", "github", "go"],
+        githubUrl: "https://github.com/jereeemyyyy/CVWO-Frontend",
+    },
+];
+
+export default projectsData;
